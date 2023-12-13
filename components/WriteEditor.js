185d1712c4675632c2ec77fe338dc92a0bae7474
minji kim <aqua9min@gmail.com>
2023-12-14 01:56:47 +0900

    dev PageScreen

--- components/WriteEditor.js
+++ components/WriteEditor.js
@@ -1,5 +1,13 @@
 import React from 'react';
-import {View, StyleSheet, TextInput} from 'react-native';
+import {View, StyleSheet, TextInput, Text} from 'react-native';
+
+function Question({route, navigation}) {
+  return (
+    <View style={styles.question}>
+      <Text style={styles.questionText}>{question.ask}</Text>
+    </View>
+  );
+}
 
 function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
   const SeparatorView = () => {
@@ -8,6 +16,8 @@ function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
 
   return (
     <View style={styles.block}>
+      <Question ask={question} />
+
       <TextInput
         placeholder="제목을 입력하세요"
         style={styles.titleInput}
@@ -15,7 +25,7 @@ function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
         onChangeText={onChangeTitle}
         value={title}
       />
-      <SeparatorView />
+
       <TextInput
         placeholder="내용을 입력하세요"
         style={styles.bodyInput}
@@ -29,24 +39,34 @@ function WriteEditor({title, body, onChangeTitle, onChangeBody}) {
 }
 
 const styles = StyleSheet.create({
-  block: {flex: 1, padding: 16},
+  block: {flex: 1},
   titleInput: {
     paddingVertical: 0,
     fontSize: 18,
     marginBottom: 16,
     color: '#263238',
     fontWeight: 'bold',
+    borderBottomWidth: 0.5,
+    flex: 1,
   },
   bodyInput: {
-    flex: 1,
     fontSize: 16,
-    paddingVertical: 0,
+    paddingTop: 10,
     color: '#263238',
+    flex: 4,
   },
   listItemSeparatorStyle: {
     height: 0.8,
     width: '100%',
     backgroundColor: '#c8c8c8',
+  },
+  question: {
+    flex: 1,
+    borderBottomWidth: 0.5,
+    justifyContent: 'center',
+  },
+  questionText: {
+    fontSize: 16,
   },
 });
 
--- screens/PageScreen.js
+++ screens/PageScreen.js
@@ -1,11 +1,44 @@
 import React from 'react';
-import {SafeAreaView, StyleSheet} from 'react-native';
+import {SafeAreaView, TextInput, StyleSheet, View, Text} from 'react-native';
 import WriteEditor from '../components/WriteEditor';
+import WriteHeader from '../components/WriteHeader';
 
-function PageScreen() {
+function Question(props) {
+  return (
+    <View style={styles.question}>
+      <Text style={styles.questionText}>{props.ask}</Text>
+    </View>
+  );
+}
+
+function PageScreen({route, navigation, title, body}) {
+  const {question} = route.params;
+  const SeparatorView = () => {
+    return <View style={styles.listItemSeparatorStyle} />;
+  };
+
   return (
     <SafeAreaView style={styles.block}>
-      <WriteEditor />
+      <WriteHeader />
+      <SeparatorView />
+      <View style={styles.block}>
+        <Question ask={question} />
+
+        <TextInput
+          placeholder="제목을 입력하세요"
+          style={styles.titleInput}
+          returnKeyType="next"
+          value={title}
+        />
+
+        <TextInput
+          placeholder="내용을 입력하세요"
+          style={styles.bodyInput}
+          multiline
+          textAlignVertical="top"
+          value={body}
+        />
+      </View>
     </SafeAreaView>
   );
 }
@@ -14,6 +47,40 @@ const styles = StyleSheet.create({
   block: {
     flex: 1,
     backgroundColor: 'white',
+  },
+  listItemSeparatorStyle: {
+    height: 0.8,
+    width: '100%',
+    backgroundColor: '#c8c8c8',
+  },
+  titleInput: {
+    paddingVertical: 0,
+    fontSize: 18,
+    marginBottom: 16,
+    color: '#263238',
+    fontWeight: 'bold',
+    borderBottomWidth: 0.5,
+    flex: 1,
+  },
+  bodyInput: {
+    fontSize: 16,
+    paddingTop: 10,
+    color: '#263238',
+    flex: 4,
+  },
+  listItemSeparatorStyle: {
+    height: 0.8,
+    width: '100%',
+    backgroundColor: '#c8c8c8',
+  },
+  question: {
+    flex: 1,
+    borderBottomWidth: 0.5,
+    justifyContent: 'center',
+  },
+  questionText: {
+    fontSize: 16,
+    marginLeft: 10,
   },
 });
 
