a2e14f2c026e0b952792129cce791bd2b8f86398
Jiyong Lee <jyle36@gmail.com>
2024-01-21 10:59:05 +0900

    context API applied

--- screens/RootStack.js
+++ screens/RootStack.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import SignInScreen from './SignInScreen';
 import MainTab from './MainTab';
@@ -6,60 +6,63 @@ import WelcomeScreen from './WelcomeScreen';
 import WebviewScreen from './WebviewScreen';
 import SignUpScreen from './SignUpScreen';
 import SetupProfileScreen from './SetupProfileScreen';
-import ListScreen from './ListScreen';
 import PageScreen from './PageScreen';
 import SettingScreen from './SettingScreen';
-import ProfileScreen from './ProfileScreen';
+import ProfileScreen from './ModifyProfileScreen';
+import {UserContext} from '../contexts/UserContext';
+import WriteScreen from './WriteScreen';
 
 const Stack = createNativeStackNavigator();
 
 function RootStack() {
+  const {user} = useContext(UserContext);
+
   return (
     <Stack.Navigator>
-      <Stack.Screen
-        name="Setting"
-        component={SettingScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="Profile"
-        component={ProfileScreen}
-        options={{headerShown: false}}
-      />
-
-      <Stack.Screen
-        name="List"
-        options={{title: '글쓰기'}}
-        component={ListScreen}
-      />
-
-      <Stack.Screen
-        name="Welcome"
-        component={WelcomeScreen}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen
-        name="SignIn"
-        component={SignInScreen}
-        options={{headerTitleAlign: 'center'}}
-      />
-      <Stack.Screen
-        name="MainTab"
-        component={MainTab}
-        options={{headerShown: false}}
-      />
-      <Stack.Screen name="WebView" component={WebviewScreen} />
-      <Stack.Screen name="SignUp" component={SignUpScreen} />
-      <Stack.Screen
-        name="SetupProfile"
-        component={SetupProfileScreen}
-        options={{headerTitleAlign: 'center'}}
-      />
-      <Stack.Screen
-        name="Page"
-        options={{title: '페이지', headerShown: false}}
-        component={PageScreen}
-      />
+      {user ? (
+        <>
+          <Stack.Screen
+            name="MainTab"
+            component={MainTab}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Page"
+            options={{title: '페이지', headerShown: false}}
+            component={PageScreen}
+          />
+          <Stack.Screen
+            name="Profile"
+            component={ProfileScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="Write"
+            options={{title: '글쓰기'}}
+            component={WriteScreen}
+          />
+        </>
+      ) : (
+        <>
+          <Stack.Screen
+            name="Welcome"
+            component={WelcomeScreen}
+            options={{headerShown: false}}
+          />
+          <Stack.Screen
+            name="SignIn"
+            component={SignInScreen}
+            options={{headerTitleAlign: 'center'}}
+          />
+          <Stack.Screen name="WebView" component={WebviewScreen} />
+          <Stack.Screen name="SignUp" component={SignUpScreen} />
+          <Stack.Screen
+            name="SetupProfile"
+            component={SetupProfileScreen}
+            options={{headerTitleAlign: 'center'}}
+          />
+        </>
+      )}
     </Stack.Navigator>
   );
 }
